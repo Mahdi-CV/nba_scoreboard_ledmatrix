3067affb0e86eac2e1477cb987627113781afd55
Mahdi <mahdi.ghodsi@auperatech.com>
2024-01-27 15:33:02 -0800

    using smaller font

--- scripts/renderer.py
+++ scripts/renderer.py
@@ -154,7 +154,7 @@ class Render:
                     away_score = next((p['score'] for p in game['awayTeam']['periods'] if p['period'] == period), 0)
                     home_score = next((p['score'] for p in game['homeTeam']['periods'] if p['period'] == period), 0)
 
-                    graphics.DrawText(canvas, self.font2, score_x, 24, graphics.Color(255, 255, 255), str(away_score))
+                    graphics.DrawText(canvas, self.font2, score_x, 28, graphics.Color(255, 255, 255), str(away_score))
                     graphics.DrawText(canvas, self.font2, score_x, 32, graphics.Color(255, 255, 255), str(home_score))
 
             elif game_status == 3:  # Game has finished
